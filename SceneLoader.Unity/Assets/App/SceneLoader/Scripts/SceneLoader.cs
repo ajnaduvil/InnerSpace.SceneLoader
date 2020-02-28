f314c68bc251f523104423bd88c7a2bfe89f4110
ajnaduvil <ajnaduvil@gmail.com>
2020-02-28 23:18:16 +0530

    Scene loader in progress

---
+++ SceneLoader.Unity/Assets/App/SceneLoader/Scripts/SceneLoader.cs
@@ -0,0 +1,38 @@
+﻿using AVR.Utils;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Innerspace.TestApp.SceneLoader
+{
+    public class SceneLoader : Singleton<SceneLoader>
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+
+        #region Scene Loader Methods
+        public void StartLoadingScene(string sceneName,LoadSceneMode sceneMode)
+        {
+           
+            var sceneloadOperation = SceneManager.LoadSceneAsync(sceneName,sceneMode,);
+            sceneloadOperation.completed += (loadOperation) =>
+            {
+
+            };
+
+        }
+
+        #endregion
+    }
+}
