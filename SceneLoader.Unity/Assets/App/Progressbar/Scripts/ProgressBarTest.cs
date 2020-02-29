cbe96e4213c1eae3271c4e2c1b027517098f5c4e
ajnaduvil <ajnaduvil@gmail.com>
2020-02-29 10:56:06 +0530

    progress bar update

---
+++ SceneLoader.Unity/Assets/App/Progressbar/Scripts/ProgressBarTest.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Innerspace.TestApp.UI;
+public class ProgressBarTest : MonoBehaviour
+{
+    public Progressbar progressbar;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(ProgressBarChangeTest());
+    }
+  
+    IEnumerator ProgressBarChangeTest()
+    {
+        while (true)
+        {
+            progressbar.Progress = Mathf.PingPong(Time.time, 1);
+            yield return null;
+        }
+    }
+}
---
+++ SceneLoader.Unity/Assets/App/Progressbar/Scripts/Progressbar.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Innerspace.TestApp.UI
+{
+
+    /// <summary>
+    /// Progressbar image that indicates the progress update
+    /// </summary>
+    public class Progressbar : MonoBehaviour
+    {
+
+        [Range(0, 1)]
+        [SerializeField]
+        private float progress;
+
+        /// <summary>
+        /// Event that gets invoked when a progressbar value changes
+        /// </summary>
+        public Action<float> progressChanged;
+
+
+        /// <summary>
+        /// The current progress value between 0 and 1. Also, updates the UI when set;
+        /// </summary>
+        public float Progress
+        {
+            get
+            {
+                return progress;
+            }
+            set
+            {
+                // Clamps the value within 0-1
+                progress = Mathf.Clamp(value, 0, 1);
+                progressChanged?.Invoke(progress);
+            }
+        }
+
+
+        private void OnValidate()
+        {
+            progressChanged?.Invoke(progress);
+        }
+    }
+}
