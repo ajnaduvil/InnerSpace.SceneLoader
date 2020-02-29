3a276d7be94b484d25d187c387de3e0af1f6848b
ajnaduvil <ajnaduvil@gmail.com>
2020-03-01 02:17:37 +0530

    fixed tooltip data config file read/write bug

--- SceneLoader.Unity/Assets/App/Modules/ToolTip/Scripts/ToolTipDataContainer.cs
+++ SceneLoader.Unity/Assets/App/Modules/ToolTip/Scripts/ToolTipDataContainer.cs
@@ -8,37 +8,31 @@ namespace Innerspace.TestApp.ToolTip
     /// <summary>
     /// Tooltip container scriptable object. Also can serialise/deserialise from a path using <see cref="LoadOrCreateConfig(string)"/>
     /// </summary>
-    [CreateAssetMenu(fileName = "ToolTipContainer", menuName = "Innerspace/TooltipData", order = 1)]    
+    [CreateAssetMenu(fileName = "ToolTipContainer", menuName = "Innerspace/TooltipData", order = 1)]
     public class ToolTipDataContainer : ScriptableObject
     {
         public ToolTipData[] toolTips;
-        static string GetConfigPath()
+  
+        private static string ReadConfigFile(string fileNamePath)
         {
-            var path = Path.Combine(Application.persistentDataPath, "ToolTipConfig.json");
-            return path;
-        }
-
-        private static string ReadConfigFile()
-        {
-            var path = GetConfigPath();
-            var sr = File.OpenText(path);
+            var sr = File.OpenText(fileNamePath);
             var data = sr.ReadToEnd();
             return data;
-
         }
 
         public void LoadOrCreateConfig(string path)
         {
-            if (File.Exists(path))
+            var fileName = Path.Combine(Application.dataPath, "ToolTipConfig.json");
+            if (File.Exists(fileName))
             {
-                var stringConfig = ReadConfigFile();
+                var stringConfig = ReadConfigFile(fileName);
                 JsonUtility.FromJsonOverwrite(stringConfig, this);
             }
             else
             {
                 Debug.Log("Config File Doesnot exists!");
                 Debug.Log("Creating config file at : " + path);
-                CreateConfig(path);
+                CreateConfig(fileName);
                 Debug.Log("Config File Created :" + path);
             }
         }
