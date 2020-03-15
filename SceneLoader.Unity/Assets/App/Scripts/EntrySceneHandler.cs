e9b26c05b6f14f2c17dfc560721a4907e6d8ba1f
ajnaduvil <ajnaduvil@gmail.com>
2020-03-16 03:44:14 +0530

    Added entry scene handler to make sure VRTK resolves the active camera before app logic starts to execute

---
+++ SceneLoader.Unity/Assets/App/Scripts/EntrySceneHandler.cs
@@ -0,0 +1,23 @@
+﻿using Innerspace.TestApp.Utils;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using VRTK;
+namespace Innerspace.TestApp
+{
+    public class EntrySceneHandler : MonoBehaviour
+    {
+        public int sceneIndexToLoad;
+
+        /// <summary>
+        /// Makes sure the main camera reference is resolved and loads init scene
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerator Start()
+        {
+            yield return new WaitUntil(() => CameraCache.Main != null);
+            yield return SceneManager.LoadSceneAsync(sceneIndexToLoad, LoadSceneMode.Additive);
+        }
+    }
+}
