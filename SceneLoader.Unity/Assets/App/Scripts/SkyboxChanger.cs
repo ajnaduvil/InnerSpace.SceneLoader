c1469a4a3e97bbc5ccf320b0e5e30ae47bed9548
ajnaduvil <ajnaduvil@gmail.com>
2020-03-16 03:41:25 +0530

    skybox chaning logic to apploaded state

---
+++ SceneLoader.Unity/Assets/App/Scripts/SkyboxChanger.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Innerspace.TestApp
+{
+    public class SkyboxChanger : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        public Material skyboxMaterial;
+
+        public void SetSkybox()
+        {
+            RenderSettings.skybox = skyboxMaterial;
+        }
+    }
+}
+
--- SceneLoader.Unity/Assets/App/Scripts/States/AppLoadedState.cs
+++ SceneLoader.Unity/Assets/App/Scripts/States/AppLoadedState.cs
@@ -1,9 +1,12 @@
 ﻿using AVR.Utils.StateMachines;
+using UnityEngine;
 
 namespace Innerspace.TestApp.States
 {
     public class AppLoadedState : MonoBehaviourState
     {
+        [SerializeField]
+        internal SkyboxChanger skyboxChanger;
         #region Singleton
         public static MonoBehaviourState Instance;
         private void Awake()
@@ -18,5 +21,12 @@ namespace Innerspace.TestApp.States
             }
         }
         #endregion
+
+        public override void OnEnter()
+        {
+            base.OnEnter();
+            skyboxChanger.SetSkybox();
+        }
+
     }
 }
