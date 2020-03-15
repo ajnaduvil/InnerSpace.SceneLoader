0b3936957afedf6e5c035ba4885acdb941506213
ajnaduvil <ajnaduvil@gmail.com>
2020-03-16 03:40:52 +0530

    Added CameraCache to Innerspace.TestApp.Utils

---
+++ SceneLoader.Unity/Assets/App/Modules/Utils/CameraCache.cs
@@ -0,0 +1,34 @@
+﻿using AVR.Utils;
+using UnityEngine;
+using VRTK;
+
+namespace Innerspace.TestApp.Utils
+{
+    /// <summary>
+    /// Finds and stores the main camera reference from VRTK
+    /// </summary>
+    public class CameraCache : Singleton<CameraCache>
+    {
+        [HideInInspector]
+        public static Camera Main { get; private set; }
+        public LayerMask mainCameraMask;
+        void Awake()
+        {
+            base.Awake();
+            VRTK_SDKManager.instance.AddBehaviourToToggleOnLoadedSetupChange(this);
+        }
+        protected virtual void OnEnable()
+        {
+            //  var activeCameraRig = GameObject.FindGameObjectWithTag("CameraRig");
+            var headTransform = VRTK_DeviceFinder.HeadsetCamera();
+
+            Main = headTransform.GetComponent<Camera>();
+            Main.cullingMask = mainCameraMask;
+        }
+        protected virtual void OnDestroy()
+        {
+            base.OnDestroy();
+            VRTK_SDKManager.instance.RemoveBehaviourToToggleOnLoadedSetupChange(this);
+        }
+    }
+}
